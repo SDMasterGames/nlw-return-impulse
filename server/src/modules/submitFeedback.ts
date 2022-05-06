c0bbbbb69bce745d0729b36384a1c269afda1f2b
SDMasterGames <sdmastergames@hotmail.com>
2022-05-06 09:38:29 -0400

    adicionando img no body do email

--- server/src/modules/submitFeedback.ts
+++ server/src/modules/submitFeedback.ts
@@ -35,7 +35,7 @@ export class submitFeedback {
         `<div style="font-family:sans-serif; font-size:16px; color:#111;" >`,
         `<p>Tipo do feedback ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
-        `<img src="${screenshot}" />`,
+        screenshot ? `<img src="${screenshot}" />` : "",
         "</div>",
       ].join("\n"),
     });
