d340550bece2e23ac7c4b647b1305cbf3db1433f
SDMasterGames <sdmastergames@hotmail.com>
2022-05-06 09:58:54 -0400

    port

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
-
+//PORT
 const PORT = process.env.PORT || 3333;
 
 const app = express();
